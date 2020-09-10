1c83394105fbeac866d8a753cb25114dd7dc0b0a
Manish Kumar <mk778281@gmail.com>
2020-09-10 16:17:43 +0530

    build file

---
+++ augle.ai/build/precache-manifest.bb189bb611acfc757791111ce847f5ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e17cec882358cf505da9b4ff6099449",
+    "url": "/index.html"
+  },
+  {
+    "revision": "506f6beb18b3bc49fdf5",
+    "url": "/static/css/main.417f9468.chunk.css"
+  },
+  {
+    "revision": "287da72c38ffe2c18472",
+    "url": "/static/js/2.8c258841.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8c258841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "506f6beb18b3bc49fdf5",
+    "url": "/static/js/main.ada2c9be.chunk.js"
+  },
+  {
+    "revision": "99a1df5724a42cc4fef3",
+    "url": "/static/js/runtime-main.4a3acf61.js"
+  }
+]);
